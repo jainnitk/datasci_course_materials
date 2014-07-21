1900957b2a7e5d9f5e40f8483f7e6a4a65e96966
ujwlkarn <ujwlkarn@gmail.com>
2014-07-21 21:43:22 +0530

    new

--- assignment1/twitterstream.py
+++ assignment1/twitterstream.py
@@ -3,10 +3,10 @@ import urllib2 as urllib
 
 # See assignment1.html instructions or README for how to get these credentials
 
-api_key = "<Enter api key>"
-api_secret = "<Enter api secret>"
-access_token_key = "<Enter your access token key here>"
-access_token_secret = "<Enter your access token secret here>"
+api_key = "m0c5PKS3ZkMOzRKpLRU3HfFxS"
+api_secret = "iG7gbwFXvFnbxREMvPQwkrCwNnUPVrPZ9svZht8ChslVFJMT95"
+access_token_key = "72530556-lciXhI3MNNDqVWMIrMedkSJiy1KJbEExK7rmGb3Uq"
+access_token_secret = "x2IHPJbxDGdnHPboxCZiFD6EKfuFlwUMDsjq8cwiDv1qe"
 
 _debug = 0
 
